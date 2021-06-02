b40fd8e5ab5a4da6e7e5c699ffd26774bf9a10b5
Rohan Gulati <rohangulati94@gmail.com>
2021-06-02 17:49:54 +0530

    Started code for bulk msg

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 const venom = require('venom-bot');
 const fs = require('fs');
 const commands = require('./config/commands.json');
@@ -7,7 +7,7 @@ const { logMessage } = require('./utils/log_message');
 //const { chatReply } = require('./services/send_message');
 const { authGoogleSheets } = require('./services/google_sheets_auth');
 
-require('./config/db')
+require('./config/db');
 
 venom
     .create(
@@ -30,66 +30,18 @@ venom
             if (commands[message.body]) {
                 runCommand(client, googleSheets, message.body);
             }
+            // for one to one 
+            // if(sender in configured oneTOOnenumber number){
+            //  call oneTonebulk method and send message text
+            // }
+
+            // for bulk group
+            // if(sender in configured bulkgroup number){
+            //  call groupbulk method
+            // }
+
             logMessage(client, googleSheets, message);
         })
-
-        // bulk join and messaging
-        // accept invite
-        // https://chat.whatsapp.com/LwtK8oNgHEJJRVYH7ajDE2
-        // https://chat.whatsapp.com/C5U7TTuY3wJ39VqrU4owoh
-        // https://chat.whatsapp.com/DLTw8AvVbLkCchmaFob36Y
-        //console.log(await client.joinGroup('DLTw8AvVbLkCchmaFob36Y'));
-        // await client
-        // .sendMessageOptions(
-        //   '919594445366@c.us',
-        //   'did you get this message ? -- Rohan',
-        //    {
-        //      // quotedMessageId: reply,
-        //     }
-        // )
-        // .then((retorno) => {
-        //   resp = retorno;
-        // })
-        // .catch((e) => {
-        //   console.log(e);
-        // });
-
-        // On new message
-        // console.log(await chatReply(client, '919729140966@c.us', 'did you get this message ? -- Rohan'));
-        //console.log(await chatReply(client, '919594445366@c.us', 'did you get this message ? -- Rohan'));
-
-        // await client
-        //     .sendText('9729140966@c.us', 'did you get this message 2? -- Rohan')
-        //     .then((result) => {
-        //         console.log('Result: ', result); //return object success
-        //     })
-        //     .catch((erro) => {
-        //         console.error('Error when sending: ', erro); //return object error
-        //     });
-
-        // getAllMessagesInChat
-        //const messages = await client.getAllMessagesInChat('919729695315-1588869495@g.us');
-        // console.log(messages);
-        //console.log('--------222222222222----------');
-
-        // Retrieve more chat message
-        //const moreMessages = await client.loadEarlierMessages('919729695315-1588869495@g.us');
-        // console.log(moreMessages);
-
-        // moreMessages.forEach(msg => {
-        //     console.log(msg.sender.formattedName, msg.sender.id, '---', msg.body);
-        // })
-        //console.log('-----------33333333333---------------')
-
-        // Retrieve all messages in chat
-        // const allMessages = await client.loadAndGetAllMessagesInChat(
-        //     '919729695315-1588869495@g.us'
-        // );
-        // // allMessages.forEach(msg => {
-        //     console.log(msg.sender.formattedName, msg.sender.id, '---', msg.body);
-        // })
-
-        //start(client);
     })
     .catch((erro) => {
         console.log(erro);
---
+++ utils/broadcast_individual.js
@@ -0,0 +1,14 @@
+// get contact list from sheet
+// format received 10 digit numbers with enable 'yes' from sheet
+// message text received as input to function
+// send message in loop with delay
+
+        // text msg send demo
+        // client
+        //     .sendText('91923456789@c.us', '👋 Demo message from. Rohan')
+        //     .then((result) => {
+        //         console.log('Result: ', result); //return object success
+        //     })
+        //     .catch((erro) => {
+        //         console.error('Error when sending: ', erro); //return object error
+        //     });
